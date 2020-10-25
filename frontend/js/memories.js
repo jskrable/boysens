6c3f1410b9e60a5e501815ffaff831ba4d33480e
Jack Skrable <jskrable@gmail.com>
2020-10-25 03:06:53 -0400

    memories working. need to add navbar. need to add favicons. need to style

--- frontend/js/memories.js
+++ frontend/js/memories.js
@@ -1,113 +1,147 @@
-// Get search query
-$( document ).ready(function() {
-		$('#submit').click((e) => {
-			query = $('#search').val();
-			console.log(query);
-			// Make sure they ask something
-			if (query.length == 0) {
-				javascript:alert('Ask me a darn question!');
-			} else {
-				// Setup lambda call
-				var params = {
-					FunctionName: 'getQuip',
-					InvocationType: 'RequestResponse',
-					LogType: 'Tail',
-					Payload: '{"query": ' + JSON.stringify(query.toLowerCase()) + '}' // search terms
-				};
-				console.log(params);
-				triggerLambda(params);
-			}
-		})
-	});
+// Get memory
+$(document).ready(function() {
 
-	// Include enters for clicks
-    $('#search').keypress((e) => {
-    if ( e.which == 13 ) {
+    checkForMemories()
+});
+
+/*// Include enters for clicks
+$('#search').keypress((e) => {
+    if (e.which == 13) {
         $('#submit').click();
-    	}	
-	});
+    }
+});*/
+
+$('#submit-memory').click((e) => {
+    console.log('submit')
+    memory = $('#memory-message').val();
+    console.log(memory);
+    // Make sure they ask something
+    if (memory.length == 0) {
+        javascript: alert('Please submit something...');
+    }
+    else {
+        // Setup lambda call
+        var params = {
+            FunctionName: 'saveMemory',
+            InvocationType: 'RequestResponse',
+            LogType: 'Tail',
+            Payload: '{"memory": ' + JSON.stringify(memory.toLowerCase()) + '}' // search terms
+        };
+        console.log(params);
+        triggerLambda(params, 'POST');
+
+        // add immediately to list here? need date and memory.
+    }
+});
 
 
-
-// display list if loaded already
-var memoryContainer = JSON.parse(sessionStorage.getItem('memoryContainer'));
-if (memoryContainer != null) {
-    popFileMenu(memoryContainer);
-    // else fetch jobs and then display
-} else if (sessionStorage.getItem('memoryContainer') === null && $("#job-list-container").length >= 1){
-    getFileJobs();
+function checkForMemories() {
+    console.log('Checking storage')
+        // display list if loaded already
+    var memoryContainer = JSON.parse(sessionStorage.getItem('memoryContainer'));
+    if (memoryContainer != null) {
+        // write this
+        displayMemories(memoryContainer);
+        // else fetch jobs and then display
+    } else {
+        getMemories();
+    }
 }
 
 
+function submitModal(response, type) {
 
-// Create modal for response display 
-function doModal(content) {
-    html =  '<div id="dynamicModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
-    html += '<div class="modal-dialog modal-dialog-centered">';
-    html += '<div class="modal-content">';
-    html += '<div class="modal-header">';
-    //html += '<a class="close ml-0" data-dismiss="modal">x</a>';
-    html += '<h6 class=" mt-0 m-auto">Please share any memories you have so we can all remember...</h4>'
-    html += '</div>'; // header
-    html += '<div class="modal-body mr-1">';
-    html += '<div class="row">';
-    html += '<div class="col-md-6 text-center m-auto" >';
-    html += ;
-    html += '</div>'; //content
-    html += '<div class="col-md-6">';
-    html += '<img src="http://unclepaulknowsall.com/css/images/crossed_arms.jpg" class="img-thumbnail rounded float-right" alt="ResponsiveImage">';
-    html += '</div>'; // image
-    html += '</div>'; // row
-    html += '</div>'; // modal
-    html += '<div class="modal-footer">';
-    html += '<span class="btn btn-primary m-auto" data-dismiss="modal">Close</span>';
-    html += '</div>';  // content
-    html += '</div>';  // dialog
-    html += '</div>';  // footer
-    html += '</div>';  // modalWindow
-    $('body').append(html);
-    $('#dynamicModal').modal();
-    $('#dynamicModal').modal('show');
+    $('#memory-message').val('');
+    $('#submit-memory-modal').modal('hide');
+    $('#submit-success-modal').modal('show');
+}
 
-    // exit modal and clear search
-    $('#dynamicModal').on('hidden.bs.modal', function (e) {
-        $(this).remove()
-        $('#search').val('');
+
+function triggerLambda(params, type) {
+
+    // Cognito pool credentials
+    AWS.config.update({
+        region: 'us-east-2'
+    });
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-2:c50cc00d-7943-4e8b-b141-518f4a48ff8a',
+    });
+
+    // Config lambda
+    var lambda = new AWS.Lambda({
+        region: 'us-east-2',
+        apiVersion: '2015-03-31'
+    });
+    // Initialize results
+    var results;
+    // Call lambda
+    lambda.invoke(params, function(error, data) {
+        if (error) {
+            prompt(error);
+            window.alert(JSON.parse(error));
+        } else {
+            window.message = JSON.parse(data.Payload);
+            console.log(message);
+
+            if (message != null) {
+                var response = message['body'];
+                console.log(response);
+                // Display results 
+                if (type == 'POST') {
+                    submitModal(response);
+                } else {
+                    displayMemories(response);
+                }
+            }
+
+        }
     });
 
 }
 
-function triggerLambda(params) {
 
-	// Cognito pool credentials
-	AWS.config.update({region: 'us-east-2'});
-	AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-		IdentityPoolId: 'us-east-2:c50cc00d-7943-4e8b-b141-518f4a48ff8a',
-	});
+function generateCard(entry) {
+    html = '<div class="card">'
+    html += '<div class="card-body">'
+    html += '<h5 class="card-title">Card title</h5>'
+    html += '<p class="card-text">' + entry.memory + '</p>'
+    html += '<p class="card-text"><small class="text-muted">' + entry.timestamp + '</small></p>'
+    html += '</div></div>'
 
-	// Config lambda
-	var lambda = new AWS.Lambda({region: 'us-east-2', apiVersion: '2015-03-31'});
-	// Initialize results
-	var results;
-	// Call lambda
-	lambda.invoke(params, function(error, data) {
-		if (error) {
-            prompt(error);
-            window.alert(JSON.parse(error));
-        } else {	
-			window.message = JSON.parse(data.Payload);
-			console.log(message);
+    return html
+}
 
-			if (message != null) {
-				var response = message['body'];
-				console.log(response);
-				// Display results 
-				doModal(response);
 
-			}
+function displayMemories(data) {
+    //console.log(data)
+    memories = $('#memory-list');
+    $('#loading').remove();
+    //console.log(JSON.parse(data))
+    entries = JSON.parse(data).Items;
+    entries.forEach(msg => {
+        // create preview cards here, with link to open full text in modal
+        card = generateCard(msg)
+        memories.append(card)
+    });
 
-		}
-	});
+    // Listener for memory clicks
+    memories.on('click', '.card', function() {
+        // SHOW/CREATE MODAL WITH FULL TEXT HERE
+    });
+
+    // Card hover animations
+    memories.on('mouseenter', '.card', function() {
+        $(this).removeClass('bg-light');
+        $(this).addClass('text-white bg-dark');
+        $(this).find('#job-title').addClass('text-white');
+    });
+
+    memories.on('mouseleave', '.card', function() {
+        $(this).removeClass('text-white bg-dark');
+        $(this).find('#job-title').removeClass('text-white');
+        $(this).addClass('bg-light');
+    });
+
 
 }
 
@@ -116,32 +150,12 @@ function triggerLambda(params) {
 // Function to get the list of file mover jobs to display
 function getMemories() {
 
-    url = 'https://elastic.snaplogic.com:443/api/1/rest/slsched/feed/BUDev/projects/IntegrationHub/getListOfFileMoves_triggered_task'
-    auth = 'Bearer h6kDURpJ0fDJgQnjfft402h3uxHqPVVN'
-    var headers = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': auth
-        }
+    var params = {
+        FunctionName: 'getMemories',
+        InvocationType: 'RequestResponse',
+        LogType: 'Tail'
     };
+    console.log(params);
+    triggerLambda(params, 'GET');
 
-    fetch(url, headers).then(
-        function(response) {
-            if (response.ok && response.status === 200) {
-                var data = response.json().then(function(data) {
-                    sessionStorage.setItem('memoryContainer', JSON.stringify(data));
-                    memoryContainer = JSON.parse(sessionStorage.getItem('memoryContainer'));
-                    popFileMenu(memoryContainer);
-                    return data;
-                });
-
-
-            } else {
-                return response.json();
-            }
-        }).catch(function(error) {
-        return error;
-    });
 }
--- middleware/handle-query.py
+++ middleware/handle-query.py
@@ -1,50 +1,27 @@
 import json
 import boto3
-import random
-import os
 import datetime
+import uuid
 
 
 def lambda_handler(event, context):
 
-    interrogatives = ['who','what','when','where','why','how','which','whose']
-    keywords = ['watch','eat','weather','name','game','win','live','are','you']
-    quips = ["Hey, Pop... Never mind, he's asleep.",
-            "You've got something on your shirt...",
-            "You can't get that on antenna TV.",
-            "You should know that! We talked about it yesterday",
-            "I want to eat, eat, eat, yablaka and banaans.",
-            "Only Grammy knows that one.",
-            "That's stupid.",
-            "I'd ask my sister, but she's an idiot"]
-    query = event['query']
-    # terms = query.split(' ')
-    score = []
-    [score.append(x) for x in interrogatives if x in query]
-    [score.append(x) for x in keywords if x in query]
-
-    update_table(query)
-
-    if 'what' and 'name' in score:
-        response = "I'm Uncle Paul. It says my name all over the screen."
-    elif 'where' and 'you' and 'live' in score:
-        response = "I live in this computer now. I don't even have internet. Pretty cool, right?"
-    else:
-        response = quips[random.randint(0,len(quips))-1]
-        #response = "You've got something on your shirt..."
+    
+    saved = update_table(event['memory'])
 
     return {
         'statusCode': 200,
-        'body': json.dumps(response)} 
+        'body': json.dumps({"message": "Submitted"})} 
 
 
-def update_table(submission):
+def update_table(memory):
 
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    history = dynamodb.Table('question-history')
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    history = dynamodb.Table('memories')
 
     item = {
-        'question': submission,
+        'id': uuid.uuid1().hex,
+        'memory': submission,
         'timestamp': str(datetime.datetime.now())
     }
 
